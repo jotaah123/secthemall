eab5cd71d74a2d7e70299f3d3b346ad267e25a7f
theMiddleBlue <menin.andrea@gmail.com>
2017-04-01 09:35:15 +0200

    changed version

--- inc/functions.sh
+++ inc/functions.sh
@@ -13,7 +13,7 @@ fi
 PASSPHRASE=$(cat ${CDIR}/passphrase)
 MYHOSTNAME=$(hostname | base64 -w0)
 MYIPADDR=$(hostname -I | base64 -w0)
-STAVERSION="secthemall/1.0.6"
+STAVERSION="secthemall/1.0.7"
 
 if [ -f /etc/timezone ]; then
 	TIMEZONE=$(cat /etc/timezone)
